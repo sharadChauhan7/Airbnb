e824b073217b26ad4905fad1f98681812c4e1935
Sharad Chauhan <sharadsingh6464@gmail.com>
2024-01-15 21:54:28 +0530

    Final Commit

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const localStrategy=require('passport-local');
 
 
 // Routs
+app.get("/",(req,res)=>{
+    res.redirect('/listings');
+})
 const listings=require("./routs/listings.js");
 const reviews=require("./routs/reviews.js");
 const auth=require("./routs/auth.js");
